257f3608be8a50c8380f5855c4e96dff1b0f4b48
zfy <rachmanino@yahoo.com>
2020-05-02 15:14:54 +0800

    update

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -13,6 +13,21 @@ dirs=(/home/travis/build/fy-travis-projects/*)
 name="$(cut -d'/' -f6 <<<"${dirs[0]}")"
 echo $name
 
+cd $HOME 
+mkdir tmp1
+cd $HOME/.m2/repository
+pwd 
+ls -al
+find . -name '*.jar' -exec mv {} $HOME/tmp1 \;
+
+cd $HOME
+mkdir tmp2
 cd $HOME/build/fy-travis-projects/$name
+find . -name '*.jar' -exec mv {} $HOME/tmp2 \;
+cd $HOME
+
 pwd
 ls -al
+
+# rsync -W -e "ssh -o StrictHostKeyChecking=no -o Compression=no" --info=progress2 tmp1 travis@35.236.128.26:/home/travis/projects/$name/
+# rsync -W -e "ssh -o StrictHostKeyChecking=no -o Compression=no" --info=progress2 tmp2 travis@35.236.128.26:/home/travis/projects/$name/
