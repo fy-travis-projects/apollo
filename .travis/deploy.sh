e8a8fc78510086718e476ada77efe7d6514f4e70
zfy <rachmanino@yahoo.com>
2020-05-03 02:37:18 +0800

    update

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -8,28 +8,31 @@ chmod o-wx ~/.ssh/
 chmod g-w ~/.ssh/config
 chmod o-wx ~/.ssh/config
 
-# get current project name
-dirs=(/home/travis/build/fy-travis-projects/*)
-name="$(cut -d'/' -f6 <<<"${dirs[0]}")"
-echo $name
-
-# collect 3rd party jars into one folder
-cd $HOME 
-mkdir lib
-cd $HOME/.m2/repository
-pwd 
-ls -al
-find . -name '*.jar' -exec mv {} $HOME/lib \;
-
-# collect build artifact jars  into one folder
-cd $HOME
-mkdir project
 cd $HOME/build/fy-travis-projects/$name
-find . -name '*.jar' -exec mv {} $HOME/project \;
-cd $HOME
-
-pwd
 ls -al
 
-rsync -W -rav -e "ssh -o StrictHostKeyChecking=no -o Compression=no" --info=progress2 lib travis@35.236.128.26:/home/travis/projects/$name/
-rsync -W -rav -e "ssh -o StrictHostKeyChecking=no -o Compression=no" --info=progress2 project travis@35.236.128.26:/home/travis/projects/$name/
+# # get current project name
+# dirs=(/home/travis/build/fy-travis-projects/*)
+# name="$(cut -d'/' -f6 <<<"${dirs[0]}")"
+# echo $name
+
+# # collect 3rd party jars into one folder
+# cd $HOME 
+# mkdir lib
+# cd $HOME/.m2/repository
+# pwd 
+# ls -al
+# find . -name '*.jar' -exec mv {} $HOME/lib \;
+
+# # collect build artifact jars  into one folder
+# cd $HOME
+# mkdir project
+# cd $HOME/build/fy-travis-projects/$name
+# find . -name '*.jar' -exec mv {} $HOME/project \;
+# cd $HOME
+
+# pwd
+# ls -al
+
+# rsync -W -rav -e "ssh -o StrictHostKeyChecking=no -o Compression=no" --info=progress2 lib travis@35.236.128.26:/home/travis/projects/$name/
+# rsync -W -rav -e "ssh -o StrictHostKeyChecking=no -o Compression=no" --info=progress2 project travis@35.236.128.26:/home/travis/projects/$name/
